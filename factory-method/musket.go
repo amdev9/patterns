fb222a2d06e8d6a1ac80088906bfd036c128d049
Alexander Matveev <>
2024-05-11 23:33:59 +0300

    add factory method

---
+++ factory-method/musket.go
@@ -0,0 +1,15 @@
+package main
+
+type musket struct {
+    Gun
+}
+
+func newMusket() IGun {
+    return &musket{
+        Gun: Gun{
+            name:  "Musket gun",
+            power: 1,
+        },
+    }
+}
+
