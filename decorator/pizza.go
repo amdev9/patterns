800f5794f66345b8bb3c36def5f97d92fd8914fa
Alexander Matveev <surfera@Alexs-MacBook-Pro.local>
2024-05-12 18:02:09 +0300

    add decorator

---
+++ decorator/pizza.go
@@ -0,0 +1,6 @@
+package main
+
+type IPizza interface {
+    getPrice() int
+}
+
---
+++ decorator/veggieMania.go
@@ -0,0 +1,9 @@
+package main
+
+type VeggieMania struct {
+}
+
+func (p *VeggieMania) getPrice() int {
+    return 15
+}
+
